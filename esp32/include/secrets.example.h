7d092338a574dc89b58ad3f418238571edbf56be
makhdanramadhan-ui <makhdanramadhan-ui@users.noreply.github.com>
2026-09-25 14:48:40 +0700

    Project PlatformIO esp32 (main.cpp + secrets terpisah)

---
+++ esp32/include/secrets.example.h
@@ -0,0 +1,7 @@
+#pragma once
+// Salin jadi secrets.h dan isi nilai asli. secrets.h di-ignore git.
+#define WIFI_SSID "GANTI_SSID"
+#define WIFI_PASSWORD "GANTI_PASSWORD"
+#define DATABASE_URL "https://GANTI_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app"
+#define DATABASE_SECRET "GANTI_DATABASE_SECRET"
+#define DEVICE_ID "cow-sprinkler-01"
